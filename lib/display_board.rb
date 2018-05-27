c1536ee427a105f5a68d09f63e316f510ce77488
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-27 19:06:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,10 @@
 board_array = [" "," "," "," "," "," "," "," "," "]
 
 def display_board
-  puts "   |   |   "
+  puts " board_array[0] | board_array[1] | board_array[2] "
   puts "-----------"
-  puts "   |   |   "
+  puts " board_array[3] | board_array[4] | board_array[5] "
   puts "-----------"
-  puts "   |   |   "
+  puts " board_array[6] | board_array[7] | board_array[8] "
+end 
+
