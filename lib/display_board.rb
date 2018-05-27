55a158d5e415b2512385c3c927777cc86579f77d
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-27 19:04:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,10 @@
 # out the current state.
 board_array = [" "," "," "," "," "," "," "," "," "]
 
-def board_move (player, space)
-  board_array['#{space -1}'] = player
-end
-
-board_move('X', 4)
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  
