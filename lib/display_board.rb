3417f824f2f287f689152cd71396d253d97ef73b
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-27 18:57:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board_array = [" "," "," "," "," "," "," "," "," "]
+
+def board_move (player, space)
+  board_array['#{space -1}'] = player
+  puts board_array
+end 
+
+board_move('X', 4)
