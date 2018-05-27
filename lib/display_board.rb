2150d7ff6b5822a6109e06c5c4fecc5cf4530b50
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-27 19:12:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board_array = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
+def display_board(board_array)
   puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
   puts "-----------"
   puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
